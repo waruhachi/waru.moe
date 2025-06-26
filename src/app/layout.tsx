1750347f2fbb2075fa0980eb6767e12f98055069
waruhachi <156133757+waruhachi@users.noreply.github.com>
2025-06-26 11:47:45 -0500

    [feat] added Analytics and Speed Insights from Vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import { ThemeProvider } from '@/components/themeProvider';
@@ -36,6 +38,8 @@ export default function RootLayout({
 	return (
 		<html className={Bumbbled.className} lang="en" suppressHydrationWarning>
 			<body>
+				<Analytics />
+				<SpeedInsights />
 				<ThemeProvider
 					attribute="class"
 					defaultTheme="system"
